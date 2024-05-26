a5d3ec27c25ae34469d1aa7d2330a9b7048a36d3
Veronika <veronika-mg@bk.ru>
2024-05-26 17:34:17 +0300

    22.2 Аутентификация

--- users/forms.py
+++ users/forms.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 
 from users.models import User
 
@@ -8,3 +8,10 @@ class UserRegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('email', 'password1', 'password2')
+
+
+class UserProfileForm(UserChangeForm):
+
+    class Meta:
+        model = User
+        fields = ('email', 'first_name', 'last_name', 'phone', ' avatar')
